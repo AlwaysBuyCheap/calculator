c7a50789744e0acaf12ef2db05ed19fc7cc0edc8
PabloTurnes <64446398+PabloTurnes@users.noreply.github.com>
2020-04-28 02:34:09 +0200

    first commit

---
+++ polls/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context
+
+
+def prueba(response):
+	doc = open("C:/Users/pablo/Desktop/calculator/mysite/html/index.html")
+	plt = Template(doc.read())
+	doc.close()
+	ctx = Context({})
+	response = plt.render(ctx)
+	return HttpResponse(response)
+
+def search(response):
+	pn = float(response.GET["pn"])
+	marketcap = float(response.GET["mc"])
+	years = int(response.GET["years"])
+	roe = float(response.GET["roe"])
+
+	discount = pn/marketcap
+	valor = (100 * discount) * (1 + roe)**years
+	valor = round(valor,2)
+
+	return HttpResponse(f"If you invest 100$ the value of the company after {years} years will be {valor}$")
+
